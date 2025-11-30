66546d9cbfe8fb07596d7a485eaeec9dc2287ea4
NicholasDewberryOfficial <85315604+NicholasDewberryOfficial@users.noreply.github.com>
2025-11-30 15:43:12 -0500

    fix the title

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ except Exception as e: st.error(f"{e}"); st.stop()
 
 def get(k): return [x.split(f'{k}_')[1] for x in cols if x.startswith(k)]
 
-st.title("Coffee Price AI")
+st.title("NCAT CSE GROUP 1 WEB DEPLOYMENT")
 c1,c2,c3=st.columns(3)
 cf=c1.selectbox("Coffee", get('coffee_name'))
 wd=c2.selectbox("Day", get('Weekday'))
